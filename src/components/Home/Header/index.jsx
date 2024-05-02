f1e38f8250a77b77f31d2891fc49f26907297987
JOAO VICTOR RESENDE RIBEIRO <JOAO6221@DIGITALCOLLEGE.LOCAL>
2024-05-02 15:26:53 -0300

    atualozação

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -1,18 +1,36 @@
+import axios from "axios";
 import { HeaderComponent } from "./style";
+import { useEffect, useState } from "react";
+
+
 
 export default function Header() {
+  const [cats, setCats] = useState([])
+  const [form, setForm] = useState({})
+  const [id, setId] = useState("")
+
+  async function getCats() {
+    const res = await axios.get("https://nxcuamed0k.execute-api.us-east-1.amazonaws.com/cats")
+    console.log(res);
+    setCats(res.data)
+  }
+
+  useEffect(() => {
+    getCats()
+  }, [])
   return (
     <HeaderComponent>
-      <h1>Cats List</h1>
-
-      <div className="buttons">
-        <button type="button" className="addCat">
-          + cat
-        </button>
-        <button type="button" className="user">
-          user
-        </button>
+      <div>
+        <form>
+          <label>Name <input type="text" id="name" name="name"/></label>
+          <label>Name <input type="text" id="name" name="name"/></label>
+          <label>Name <input type="text" id="name" name="name"/></label>
+          <label>Name <input type="text" id="name" name="name"/></label>
+        </form>
       </div>
+
+
+
     </HeaderComponent>
   );
 }
